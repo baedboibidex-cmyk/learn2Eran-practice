640fe59d5b046ecf6a2630432ef762653e5ccc0a
OLUBUDO PROMISE <baedboibidex@gmail.com>
2026-05-05 10:46:01 +0100

    oluwabhidex

---
+++ letslearnstring/go.mod
@@ -0,0 +1,3 @@
+module letslearnstring
+
+go 1.22.2
---
+++ letslearnstring/main.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main(){
+var username string = "polubudo"
+var favouriteInt int = 9
+var phoneLenght float64 = 6.66
+var IsAfrican bool = true
+
+fmt.Println(username)
+fmt.Println(favouriteInt)
+fmt.Println(phoneLenght)
+fmt.Println(IsAfrican)
+}
